e5d6596f9f864a4d59f7e1d728c7d9bf4ab3ff8b
Luca <luca.luxxon@gmail.com.com>
2026-05-16 19:18:42 +0200

    Update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.example.tierdex"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.1.3"
+        versionCode = 6
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["MAPS_API_KEY"] = mapsApiKey
